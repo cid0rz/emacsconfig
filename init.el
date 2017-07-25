29678fefe02c444345dcac942ff324d58862ffba
cid0rz <cid.kampeador@gmail.com>
2017-07-26 00:54:08 +0200

    emacs basic configuration everywhere

---
+++ init.el
@@ -0,0 +1,91 @@
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+
+(require 'package)
+
+(add-to-list 'package-archives
+     '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(better-defaults
+    elpy
+    flycheck
+    material-theme
+    py-autopep8
+    magit
+    arduino-mode
+    use-package))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;;ORG-MODE
+
+(require 'org)
+
+;;LOAD LANGUAGES FOR CODEBLOCKS
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((C . t) (python . t)))
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers globally
+(global-set-key "\C-x\C-m" 'execute-extended-command) ;; add binding for M-x 
+(global-set-key "\C-c\C-m" 'execute-extended-command) ;; add binding for M-x
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+;; PYTHON CONFIGURATION
+;; --------------------------------------
+
+(elpy-enable)
+
+;; use flycheck not flymake with available languages
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;; enable autopep8 formatting on save
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;; allow easy switching between python envs
+;; from conda
+
+(setenv "WORKON_HOME" "/home/cid0rz/miniconda2/envs")
+
+
+;; LISP CONFIGURATION
+(add-hook 'emacs-lisp-mode-hook
+              (lambda ()
+                ;; Use spaces, not tabs.
+                (setq indent-tabs-mode nil)
+                ;; Keep M-TAB for `completion-at-point'
+                (define-key flyspell-mode-map "\M-\t" nil)
+                ;; Pretty-print eval'd expressions.
+                (define-key emacs-lisp-mode-map
+                            "\C-x\C-e" 'pp-eval-last-sexp)
+                ;; Recompile if .elc exists.
+                (add-hook (make-local-variable 'after-save-hook)
+                          (lambda ()
+                            (byte-force-recompile default-directory)))
+                (define-key emacs-lisp-mode-map
+                            "\r" 'reindent-then-newline-and-indent)))
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode) ;; Requires Ispell
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+
+;; init.el ends here
