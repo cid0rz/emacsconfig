9e588027ca2ac5de70732a95de5a617a3ed6ce86
cid0rz <cid.kampeador@gmail.com>
2018-10-12 23:19:58 +0200

    improved version

--- init.el
+++ init.el
@@ -26,7 +26,6 @@
 (add-to-list 'package-archives
      '("melpa" . "http://melpa.org/packages/") t)
 
-(package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -37,6 +36,9 @@
     material-theme
     py-autopep8
     magit
+    dockerfile-mode
+    yaml-mode
+    docker-compose-mode
     arduino-mode
     use-package))
 
@@ -110,3 +112,16 @@
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 
 ;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(docker-compose-mode yaml-mode dockerfile-mode use-package arduino-mode magit py-autopep8 material-theme flycheck elpy better-defaults)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
