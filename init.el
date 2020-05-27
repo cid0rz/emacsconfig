9add1526bf45ed606045b0409c89560e553bb8b1
cid0rz <cid.kampeador@gmail.com>
2020-05-27 22:01:44 +0200

    vterm added

--- init.el
+++ init.el
@@ -152,6 +152,9 @@
        ((t (:inherit ace-jump-face-foreground :height 3.0)))))))
 
 
+(use-package vterm
+    :ensure t)
+
 ;;; init.el ends here
 
 (custom-set-variables
@@ -167,4 +170,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
