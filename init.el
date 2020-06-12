69a02ce92f42cd1964bda4cb7f2c051a555f203f
cid0rz <cid.kampeador@gmail.com>
2020-06-12 13:28:18 +0200

    properly bootstrap use-package

--- init.el
+++ init.el
@@ -39,10 +39,11 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
 ;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
